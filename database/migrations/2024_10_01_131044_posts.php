25bfc54f757d0a7f19b4a4ae7cc8cc98dee288ea
mokkiiiiiiii <hiroto.m.0926@gmail.com>
2024-10-01 22:30:11 +0900

    Initial commit

---
+++ database/migrations/2024_10_01_131044_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('posts', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('user_name', 255);
+        $table->string('post', 255);
+        $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+      });  //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::drop('posts');  //
+    }
+}
