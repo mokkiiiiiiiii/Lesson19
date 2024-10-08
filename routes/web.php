a9c733cee968ab85dc5ae0257d203b532808c1d8
mokkiiiiiiii <hiroto.m.0926@gmail.com>
2024-10-08 11:02:21 +0900

    新規登録完了画面はまだ。他は大体できている

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\PostsController;
 use Illuminate\Http\Request;
 
 
-
-
 // ルートURLにアクセスされたときにログインページにリダイレクト
 Route::get('/', function () {
     return redirect()->route('login');
@@ -17,8 +15,30 @@ Route::get('/', function () {
 //name部分はルートに名前を付与。URLのパスが変更された場合でも、ルート名を使ってそのルートにアクセスできるようになる
 Route::get('index', [PostsController::class, 'index'])->name('posts.index');
 
+Route::get('/create-form', [PostsController::class, 'createForm'])->name('posts.createForm');
 
+//create-formで入力した情報が送信されるとき、このルートが実行される。PostsControllerのcreateメソッドを実行するよう指定
+Route::post('/post/create', [PostsController::class, 'create'])->name('posts.create');
 
+//idには投稿を識別するための任意の数値が入る。そのidの投稿に関する更新フォームが表示
+Route::get('post/{id}/update-form', [PostsController::class, 'updateForm'])->name('posts.edit');
+
+//update-formで入力した情報が送信されるとき、このルートが実行される。PostsControllerのupdateメソッドを実行するよう指定
+Route::post('/post/update', [PostsController::class, 'update']);
+
+//特定の投稿を識別し、ボタン押下時このルートが実行される。PostsControllerのdeleteメソッドを実行するよう指定
+Route::get('post/{id}/delete', [PostsController::class, 'delete']);
+
+//検索ボタン押下時このルートが実行される。PostsControllerのメソッドを実行するよう指定
+Route::get('/posts/search', [PostsController::class, 'search'])->name('posts.search');
+
+//postリクエストによりルートに定義された処理が実行
+//Auth::logoutで、Laravelの認証システムを使って現在ログインしているユーザーをログアウト
+//ログアウト後にユーザーを/loginページにリダイレクト
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect('/login');
+})->name('logout');
 
 
 //ユーザー認証に関連する一連のルートを自動的に登録するためのもの
